d26f706a72ad0ef0bb99091266c583f3eecd53ff
GustavoPinheiro170 <gustavocampos170@hotmail.com>
2020-09-11 22:34:50 -0300

    Git init

--- src/App.js
+++ src/App.js
@@ -26,8 +26,8 @@ function App() {
     async function handleClick(event) {
       const response = await fetch(`https://corona-api.com/countries/${event.target.value}`
       ).then((response) => { return response; }).then((body) => { return body.json(); })
-      setDados(response);
-
+      setDados(response); 
+ 
     }
 
   }, []); 
